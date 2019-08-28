daf9049c6e1b2eb901614539b7dd35d5af9dee27
Rajat Sharma <rajsharm@microsoft.com>
2019-08-28 14:00:18 +0530

    d

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,8 +13,8 @@ class Quiz extends Component {
     {
         return (
             <div>
-                <div className="QuizQuestion"> {quizData.quiz_questions[0].instruction_text}</div>
-            </div>
+                <div className="QuizQuestion"> {quizData.quiz_questions[0]
+                    .instruction_text}</div></div>
             )
     }
 
